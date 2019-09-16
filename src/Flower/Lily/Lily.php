0174e3b50f70741af7e5742b2931ab80b0cf0c33
linjiewen <linjiewen@dyg.cn>
2019-09-16 10:56:29 +0800

    first commit

---
+++ src/Flower/Lily/Lily.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linjiewen
+ * Date: 2019/9/16
+ * Time: 10:25
+ */
+
+namespace Flower\Lily;
+
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose/Rose.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linjiewen
+ * Date: 2019/9/16
+ * Time: 10:25
+ */
+
+namespace Flower\Rose;
+
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
